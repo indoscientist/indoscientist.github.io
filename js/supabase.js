3635ec128ae8a2a94bd8e554a21c81921082b57b
indoscientist <asep.dpm@gmail.com>
2026-06-30 10:37:28 +0700

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+// Nanti isi setelah project Supabase dibuat.
+// Contoh:
+// import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+// const supabaseUrl = 'https://PROJECT_ID.supabase.co'
+// const supabaseAnonKey = 'ANON_KEY_KAMU'
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey)
