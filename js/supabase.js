e3b2c597bc6e5724bd59d4cbf408da9da1549aff
indoscientist <asep.dpm@gmail.com>
2026-06-30 10:57:35 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+const SUPABASE_URL = "https://ezsdpkngbwnmtuldohtq.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_mIQwFgnpPCYGM_tkcJiruw_r1pYBL9X";
 
-// Ganti dua nilai ini dari Supabase Project Settings > API.
-export const supabaseUrl = 'https://ezsdpkngbwnmtuldohtq.supabase.co';
-export const supabaseAnonKey = 'sb_publishable_mIQwFgnpPCYGM_tkcJiruw_r1pYBL9X';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
